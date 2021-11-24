501c64fe4fa106b95056253731495669a50aba0f
Zac Stanley <56239508+zac-stanley@users.noreply.github.com>
2021-11-23 16:19:11 -0800

    update process-hydro.js, create simplifyserver.html, update README.md

--- build-scripts/process-hydro.js
+++ build-scripts/process-hydro.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const turf = require('@turf/turf)');
+const turf = require('@turf/turf');
 
 // read the California hydrography file 
-fs.readFile(__dirname + '/../project-files/california-rivers.json', 'utf-8', (err, data) => {
+fs.readFile(__dirname + '/../project-files/wbdhu12.json', 'utf-8', (err, data) => {
     if (err) throw err;
 
     console.log('file read');
@@ -13,16 +13,16 @@ fs.readFile(__dirname + '/../project-files/california-rivers.json', 'utf-8', (er
     console.log('successfully parsed')
 
     // set Turf simplify options
-    const options = {tolerance: 0.01, highQuality: false};
+    const options = {tolerance: .01, highQuality: false};
 
     // declare variable and call simplify function
     const simplified = turf.simplify(caRivers, options);
-    //const outFileName = 'C:\NewMapsPlus\Map675\675-assignment-03\project-files';
+    const caSimpleWater = 'C:/NewMapsPlus/Map675/675-assignment-03/data/wbdhu12-simple.json';
 
     // create new file with writeFile and stringify functions from files that have been simplified
-    fs.writeFrile(__dirname + '/../data/caSimpleRivers.json', JSON.stringify(simplified), 'utf-8', (err) => {
+    fs.writeFile(caSimpleWater , JSON.stringify(simplified), 'utf-8', (err) => {
         if (err) throw err;
-        console.log(outFileName + 'written to file!')
+        console.log(caSimpleWater  + ' written to file!')
     });
 
 });
